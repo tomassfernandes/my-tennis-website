4e68c7f4734378fd90e3d73b8d7812b3120ff971
Tomás Fernandes <fernandesstomas@gmail.com>
2023-10-30 16:24:19 +0000

    run build (to appear on netlify)

--- src/Sections.js/ClubSection.js
+++ src/Sections.js/ClubSection.js
@@ -2,7 +2,9 @@ export default function ClubSection() {
   return (
     <section className="club-section">
       <div className="club-section-grid">
-        <div className="club-section-text-div"></div>
+        <div className="club-section-text-div">
+          <h2></h2>
+        </div>
         <div className="club-section-img-div">
           <img
             className="club-section-img"
