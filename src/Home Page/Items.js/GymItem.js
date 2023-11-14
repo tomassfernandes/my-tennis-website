ea395677b2eee974cb5e34f98c38246aa9f94bd4
Tomás Fernandes <fernandesstomas@gmail.com>
2023-11-14 23:12:31 +0000

    HomePage queries adjustment

--- src/Home Page/Items.js/GymItem.js
+++ src/Home Page/Items.js/GymItem.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Lottie from "lottie-react";
 import animationData from "../../assets/gym-animation.json";
 import GymBenefitsItem from "./GymBenefitsItem";
@@ -6,6 +6,27 @@ import ScrollTrigger from "react-scroll-trigger";
 
 export default function GymItem() {
   const [isAnimationVisible, setIsAnimationVisible] = useState(false);
+
+  const handleScroll = () => {
+    const element = document.querySelector(".gym-lottie");
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      const windowHeight =
+        window.innerHeight || document.documentElement.clientHeight;
+      const visibility = (windowHeight - rect.top) / windowHeight;
+
+      if (visibility >= 0.6 && !isAnimationVisible) {
+        setIsAnimationVisible(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div className="gym-item-div">
@@ -19,10 +40,7 @@ export default function GymItem() {
           <img className="gym-img" src="/img/gym2.webp" alt="Gym space" />
         </div>
         <div className="gym-item-text-div">
-          <ScrollTrigger
-            className="gym-lottie"
-            onEnter={() => setIsAnimationVisible(true)}
-          >
+          <ScrollTrigger className="gym-lottie">
             {isAnimationVisible && (
               <Lottie
                 loop={false}
